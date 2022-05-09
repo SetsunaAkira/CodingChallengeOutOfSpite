7ff527def240889ae9dc87f7b0c1d18c1f41b55a
Colby <thedarkness435@gmail.com>
2022-05-09 09:11:08 -0500

    Add project files.

---
+++ DumbAwardCoTest/ParseArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumbAwardCoTest
+{
+    internal class ParseArray
+    {
+        public List<int> arrayToList = new List<int>();
+        public int[] ParseArrayForUniqueNumbers(int[] array)
+        {
+            List<int> result = new List<int>();
+            arrayToList = array.ToList();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(result.Count != 0)
+                {
+                    if (!result.Contains(array[i]))
+                    {
+                        result.Add(array[i]);
+                        arrayToList.RemoveAt(arrayToList.IndexOf(array[i]));
+                    }
+                }
+                else
+                {
+                    result.Add(array[i]);
+                    arrayToList.RemoveAt(i);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ DumbAwardCoTest/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DumbAwardCoTest;
+int[] test = { 1, 2, 5, 5, 4, 1, 4, 6, 8, 3, 9, 10, 5, 1, 4, 5,5,5,5,5,5,5,5};
+
+ParseArray parse = new ParseArray();
+int[] expected = parse.ParseArrayForUniqueNumbers(test);
+
+Console.WriteLine("Out of the numbers in test array: ");
+
+Console.WriteLine("There are " + expected.Length + " unique numbers");
+Console.WriteLine("those numbers being:");
+foreach (int numbers in expected)
+{
+    Console.WriteLine(numbers);
+}
+
+Console.WriteLine("total Duplicates: " + parse.arrayToList.Count + " Duplicates being:");
+foreach (int number in parse.arrayToList)
+{
+    Console.WriteLine(number);
+}
+Console.ReadLine();
+
+
+
+
